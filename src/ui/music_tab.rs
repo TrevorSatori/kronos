c03f4087c50cc66d285457a1c76802fd4e69cd60
taro <taro@taro.codes>
2024-08-02 22:37:13 -0300

    feat + refactor: top_bar

--- src/ui/music_tab.rs
+++ src/ui/music_tab.rs
@@ -1,18 +1,60 @@
-use std::time::Duration;
-use ratatui::{layout::{Alignment, Constraint, Direction, Layout, Rect}, style::{Modifier, Style}, text::{Text}, widgets::{Block, BorderType, Borders, Gauge, List, ListItem}, Frame};
-use ratatui::style::Color;
-use ratatui::text::{Line, Span};
+use ratatui::{
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Modifier, Style, Color},
+    text::{Text, Line, Span},
+    widgets::{Block, BorderType, Borders, List, ListItem, Padding},
+    Frame,
+};
 use ratatui::widgets::block::Position;
-use ratatui::widgets::{Padding};
+
 use crate::app::{App, InputMode};
 use crate::config::Config;
 use crate::helpers::gen_funcs;
 
+fn top_bar<'a>(app: &mut App, cfg: &Config) -> Block<'a> {
+    let folder_name = app.last_visited_path.file_name().map(|s| s.to_str()).flatten().map(String::from).unwrap_or("".to_string());
+
+    let browser_title = match app.input_mode() {
+        InputMode::BrowserFilter =>
+            Line::from(vec![
+                Span::styled(
+                    "  search: ",
+                    Style::default()
+                ),
+                Span::styled(
+                    app.browser_filter.clone().unwrap_or("".to_string()),
+                    Style::default().fg(Color::Red)
+                ),
+            ]),
+        _ => Line::from(folder_name),
+    };
+
+    let top_bar = Block::default()
+        .borders(Borders::NONE)
+        .title(browser_title)
+        .title_alignment(Alignment::Left)
+        .title_position(Position::Top)
+        .title_style(Style::new()
+            .bg(cfg.background())
+            .fg(cfg.foreground())
+        );
+
+    top_bar
+}
+
 pub fn music_tab(frame: &mut Frame, app: &mut App, chunks: Rect, cfg: &Config) {
+    let vertical_areas = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(2), Constraint::Min(1)].as_ref())
+        .horizontal_margin(2)
+        .split(chunks);
+
+    frame.render_widget(top_bar(app, cfg), vertical_areas[0]);
+
     let main_layout = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-        .split(chunks);
+        .split(vertical_areas[1]);
 
     let browser_items: Vec<ListItem> = app
         .browser_items
@@ -27,34 +69,9 @@ pub fn music_tab(frame: &mut Frame, app: &mut App, chunks: Rect, cfg: &Config) {
         })
         .collect();
 
-    let folder_name = app.last_visited_path.file_name().map(|s| s.to_str()).flatten().map(String::from).unwrap_or("".to_string());
-
-    let browser_title = match app.input_mode() {
-        InputMode::BrowserFilter =>
-            Line::from(vec![
-                Span::styled(
-                    "  search: ",
-                    Style::default()
-                ),
-                Span::styled(
-                        app.browser_filter.clone().unwrap_or("".to_string()),
-                        Style::default().fg(Color::Red)
-                ),
-            ]),
-        _ => Line::from(format!(" {}", folder_name)),
-    };
-
     let browser_block = Block::default()
         .borders(Borders::RIGHT)
-        .title(browser_title)
-        .title_alignment(Alignment::Left)
-        .title_position(Position::Top)
-        .title_style(Style::new()
-            .bg(cfg.background())
-            .fg(cfg.foreground())
-            .add_modifier(Modifier::HIDDEN))
-        .border_type(BorderType::Double)
-        .padding(Padding::new(1, 1, 1, 2));
+        .border_type(BorderType::Double);
 
     app.browser_items.height = browser_block.inner(main_layout[0]).height;
 
@@ -84,9 +101,7 @@ pub fn music_tab(frame: &mut Frame, app: &mut App, chunks: Rect, cfg: &Config) {
         .block(
             Block::default()
                 .borders(Borders::NONE)
-                .title("")
-                .title_alignment(Alignment::Center)
-                .padding(Padding::new(1, 1, 1, 1))
+                .padding(Padding::left(1))
         )
         .style(Style::default().fg(cfg.foreground()))
         .highlight_style(
