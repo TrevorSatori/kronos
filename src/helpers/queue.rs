837549a8ad6a0ee9605ca71c9edc310db839666b
taro <taro@taro.codes>
2024-08-16 16:49:57 -0300

    mini refactor

--- src/helpers/queue.rs
+++ src/helpers/queue.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 use std::{collections::VecDeque, path::PathBuf};
-
+use lofty::LoftyError;
 use super::gen_funcs::{path_to_song, path_to_song_list, Song};
 
 pub struct Queue {
@@ -16,9 +16,9 @@ fn song_list_to_duration(items: &VecDeque<Song>) -> Duration {
 impl Queue {
     pub fn new(queue: Vec<String>) -> Self {
         let mut songs: VecDeque<Song> = VecDeque::new();
+        let mut paths: VecDeque<PathBuf> = queue.iter().map(PathBuf::from).collect();
 
-        for path in queue {
-            let path = PathBuf::from(path);
+        for path in paths {
             let song = path_to_song(&path);
             match song {
                 Ok(song) => { songs.push_back(song); }
