c2cb54b020fe93d798a19dcc42374cfe4bd5befe
taro <taro@taro.codes>
2024-08-21 18:23:00 -0300

    refactor: quit future: encapsulate a bit more

--- src/main.rs
+++ src/main.rs
@@ -32,24 +32,28 @@ pub enum Command {
 async fn main() -> Result<(), Box<dyn Error>> {
     std::panic::set_hook(Box::new(on_panic));
 
-    let quit = Quit::new();
     let (player_command_sender, player_command_receiver) = channel();
 
-    run_player_thread(player_command_receiver, quit.state());
+    let quit = run_player_thread(player_command_receiver);
     run_mpris(player_command_sender, quit).await?;
 
     Ok(())
 }
 
-fn run_player_thread(player_command_receiver: Receiver<Command>, quit: Arc<Mutex<QuitState>>) {
+fn run_player_thread(player_command_receiver: Receiver<Command>) -> Quit {
+    let quit = Quit::new();
+    let quit_state = quit.state();
+
     thread::spawn(move || {
         if let Err(err) = run_player(player_command_receiver) {
             eprintln!("error :( {:?}", err);
         }
 
-        let mut quit = quit.lock().unwrap();
+        let mut quit = quit_state.lock().unwrap();
         quit.complete();
     });
+
+    quit
 }
 
 fn run_player(player_command_receiver: Receiver<Command>) -> Result<(), Box<dyn Error>> {
