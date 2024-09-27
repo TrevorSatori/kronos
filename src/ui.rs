f015ed6bd4905cd001fc43a8be94368124b16280
taro <taro@taro.codes>
2024-09-27 00:38:52 -0300

    rename

--- src/ui.rs
+++ src/ui.rs
@@ -1,13 +1,13 @@
 mod help_tab;
 pub mod stateful_list;
-mod ui;
+mod currently_playing;
 mod playlist;
 mod library;
 mod keyboard_handler;
 mod top_bar;
 
 pub use help_tab::*;
-pub use ui::*;
+pub use currently_playing::*;
 pub use playlist::*;
 pub use library::*;
 pub use keyboard_handler::{KeyboardHandler, KeyboardHandlerEnum, KeyboardHandlerMut};
